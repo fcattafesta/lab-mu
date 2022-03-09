663c8bdd1295b33ed548653672c217c71dfe2678
rubenforti <rubenforti27@gmail.com>
2022-03-09 12:19:36 +0100

    log update + stop/start rate code

---
+++ data/stop_rate.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+counts_path = os.path.join(os.path.dirname(__file__), 'iron')
+
+w, start, stop = np.loadtxt('iron/stop_rate.txt', unpack=True)
+
+
+
+r = (stop[:] / start[:])
+dr = np.sqrt(stop[:] * (start[:] - stop[:])/ start[:]**3)
+
+'''
+X = np.stack(w, r, dr], axis=1)
+
+np.savetxt(os.path.join(counts_path, '..', 'efficiencies',
+           'working_efficiencies.txt'), X, fmt='%.3f')
+'''
+
+plt.title('STOP/START rate')
+plt.errorbar(w,  r, yerr=dr, ls='', fmt='.', color='black')
+plt.grid(which='both', ls='--', alpha=0.5)
+plt.xlabel('Stop/Start')
+plt.ylabel('Width [cm]')
+plt.savefig('iron/stop_rate.png')
+plt.show()
