aea45db87bb300f6fbd49a73b23f9f8b624e1311
fcattafesta <f.cattafesta@studenti.unipi.it>
2022-03-25 16:22:35 +0100

    New stop_rate plots

---
+++ data/aluminum/stop_rate.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import ROOT
+
+T = 10
+
+counts_path = os.path.join(os.path.dirname(__file__), 'stop_rate.txt')
+data_path = fig_path = os.path.join(os.path.dirname(
+    __file__), 'acquisitions', 'stop_rate.txt')
+
+fig_path = os.path.join(os.path.dirname(__file__), 'figures', 'stop_rate.png')
+width, _, stop = np.loadtxt(counts_path, unpack=True)
+
+dwidth = np.ones_like(width) * 0.1
+stop = stop / T
+dstop = np.sqrt(width)
+
+X = np.stack([width, stop, dwidth, dstop], axis=1)
+np.savetxt(data_path, X, fmt='%f')
+
+c = ROOT.TCanvas('c')
+c.SetGrid()
+g = ROOT.TGraphErrors(data_path)
+g.SetMarkerStyle(21)
+g.SetTitle('Aluminum; Width [cm]; Rate [Hz]')
+g.Draw("AP")
+c.Update()
+c.SaveAs(fig_path)
+input()
