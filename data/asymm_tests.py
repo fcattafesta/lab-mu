56ae424b214068a2ee12e7a53dcc0cf3182578de
rubenforti <rubenforti27@gmail.com>
2022-04-03 01:59:28 +0200

    KS test improved

--- data/asymm_tests.py
+++ data/asymm_tests.py
@@ -7,10 +7,15 @@ import os
 t1, as1 = np.loadtxt("mag/Asymmetry_mag.txt", unpack=True)
 t2, as2 = np.loadtxt("iron/Asymmetry_iron.txt", unpack=True)
 
-print(len(t1), len(t2))
 
-as1 = np.abs(as1)
-as2 = np.abs(as2)
+l1 = len(t1)
+l2 = len(t2)
+
+print(l1, l2)
+
+
+#as1 = np.abs(as1)
+#as2 = np.abs(as2)
 
 
 n1 = as1.sum()
@@ -35,29 +40,36 @@ for i in range(0, l):
     D_vec[i] = np.abs(C1-C2)
 
 
-D_N = np.max(D_vec)*np.sqrt((n1*n2)/(n1+n2))
+D_N = np.max(D_vec)*np.sqrt((l1*l2)/(l1+l2))
 
 limit = 1.36
 
 print("D_N = ", D_N)
 print("q_alpha = ", limit)
 
-'''
-##  Test di KS sul campione non magnetizzato
 
-D_2 = np.zeros(l)
+##  Test di KS sui singoli campioni, a confronto con una distribuzione uniforme
+
+offset = 10000
+
+
+D_1 = np.zeros(len(t2))
+D_2 = np.zeros(len(t2))
 
 for i in range(0, l):
-    C3 = 0
+    C1 = 0
+    C2 = 0
     for idx in range(0, i+1):
-        C2 = C2 + as2[idx]/n2
+        if (as1[idx] != 58):
+            C1 = C1 + as1[idx]/n1
+        if (as2[idx] != 58):
+            C2 = C2 + as2[idx]/n2
         idx += 1
-    C0 = 10*i/(10*len(t1))
-    D_2[i] = np.abs(C1-C0)
+    C0 = offset*i/(offset*len(t1))
+    D_1[i] = np.abs(C1-C0)
+    D_2[i] = np.abs(C2-C0)
 
 
-limit = 1.36
-
-print("D_N_2 = ", max(D_2))
-print("q_alpha = ", limit)
-'''
+print("Critical region: Dn > ", 0.338)
+print("D1 = ", max(D_1))
+print("D2 = ", max(D_2))
